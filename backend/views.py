c32f563b629abca371ccc4bb666cf45838df5992
duong-vo <vodh2@miamioh.edu>
2023-09-09 17:23:36 -0400

    add user articles endpoint

--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view, parser_classes
-
-
 from rest_framework import status
-from backend.models import Keywords, Users, UserKeyword
+from backend.models import Keywords, Users, UserKeyword, UserArticle, Articles
+from backend.serializers import ArticlesSerializer
+from django.http.response import JsonResponse
 
 @api_view(['POST'])
 def add_user(request):
@@ -28,7 +28,6 @@ def add_user(request):
                 saved_keyword = list(Keywords.objects.filter(name = keyword))[0]
 
             saved_user_keyword = UserKeyword.objects.create(user=saved_user, keyword=saved_keyword, isActive=True)
-
         return Response({"message": "User added successfully"}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
@@ -43,17 +42,13 @@ def add_keyword(request):
             saved_keyword = Keywords.objects.create(name=keyword)
         else:
             saved_keyword = list(Keywords.objects.create(user=user, keyword=keyword, isActive=True))
-        
 
-
+@api_view(['GET'])
 def user_articles(request, user_id):
-    # Get the user object based on the user_id
-    user = User.objects.get(pk=user_id)
-    # Filter the articles for the specific user
-    articles = UserArticle.objects.filter(author=user)
-    # You can then pass the 'articles' queryset to a template for rendering
-    context = {
-        'user': user,
-        'articles': articles,
-    }
-    return render(request, 'user_articles.html', context)
+    user = Users.objects.get(pk=user_id)
+    user_articles = UserArticle.objects.filter(user=user)
+    article_ids = user_articles.values_list('article_id', flat=True)
+    articles = Articles.objects.filter(id__in=article_ids)
+    articles_serializer = ArticlesSerializer(articles, many=True)
+    print(articles_serializer.data)
+    return JsonResponse(articles_serializer.data, safe=False, status=status.HTTP_200_OK)
