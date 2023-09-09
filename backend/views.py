f621887ec6bc7cd4b4e357945cc55f785b182690
dominhnhut01 <dominhnhut01@gmail.com>
2023-09-09 18:04:27 -0400

    update requirement

--- backend/views.py
+++ backend/views.py
@@ -4,23 +4,22 @@ from rest_framework.decorators import api_view, parser_classes
 
 
 from rest_framework import status
-from backend.models import Keywords, Users, UserKeyword
+from backend.models import Keywords, Users, UserKeyword, UserArticle
+import ast
 
 @api_view(['POST'])
 def add_user(request):
     if request.method == 'POST':
         print("Receive POST request")
-        print(request)
-        print(request.data)
         fname, lname = request.data['firstname'], request.data['lastname']
         email = request.data['email']
         saved_user = Users.objects.create(firstname=fname, lastname=lname, email=email)
-        print('ok')
 
-        keywords = request.data['keywords']
-        print(keywords)
+        keywords = ast.literal_eval(request.data['keywords'])
+        
 
         for keyword in keywords:
+            print(keyword)
             keyword = keyword.strip().lower()
             if not Keywords.objects.filter(name=keyword).exists():
                 saved_keyword = Keywords.objects.create(name=keyword)
@@ -32,28 +31,28 @@ def add_user(request):
         return Response({"message": "User added successfully"}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
-def add_keyword(request):
+def add_keyword(request, user_id):
     if request.method == 'POST':
-        keyword_data = JSONParser().parse(request)
-        keyword, uid = keyword_data['keyword', 'user_id']
+        keyword= request.data['keyword']
 
         keyword = keyword.strip().lower()
-        user = Users.objects.get(id=uid)
+        user = Users.objects.get(id=user_id)
         if not Keywords.objects.filter(name=keyword).exists():
             saved_keyword = Keywords.objects.create(name=keyword)
-        else:
-            saved_keyword = list(Keywords.objects.create(user=user, keyword=keyword, isActive=True))
+            saved_userkeyword = UserKeyword.objects.create(user=user, keyword=saved_keyword, isActive=True)
+        return Response({"message": "Keyword added successfully"}, status=status.HTTP_200_OK)
+
         
 
 
 def user_articles(request, user_id):
     # Get the user object based on the user_id
-    user = User.objects.get(pk=user_id)
+    user = Users.objects.get(pk=user_id)
     # Filter the articles for the specific user
-    articles = UserArticle.objects.filter(author=user)
+    articles = UserArticle.objects.filter(user=user)
     # You can then pass the 'articles' queryset to a template for rendering
     context = {
         'user': user,
         'articles': articles,
     }
-    return render(request, 'user_articles.html', context)
+    return Response({f'{context}'}, status=status.HTTP_200_OK)
--- pennapps/urls.py
+++ pennapps/urls.py
@@ -21,6 +21,6 @@ from backend import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/add', views.add_user),
-    path('user/add_keyword', views.add_keyword),
-    path('user/<int:user_id>/articles/', views.user_articles, name='user_articles'),
+    path('user/<int:user_id>/add_keyword', views.add_keyword),
+    path('user/<int:user_id>/articles', views.user_articles, name='user_articles'),
 ]
